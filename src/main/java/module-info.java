835808637b7459925771c1fbdbed5c1651cf600b
shirotto <antonio.sabatini63@gmail.com>
2024-12-12 11:21:24 +0100

    Javafx funzionante

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.gallery.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.gallery.gui to javafx.fxml;
+    exports com.gallery.gui;
+}
