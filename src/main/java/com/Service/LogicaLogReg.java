0df303e715e4ac4d8cb3738da8d6a067e7cd76d9
Mario <mario.ranalli@student.univaq.it>
2024-12-17 02:51:33 +0100

    Aggiunta classe LogicaLogReg

--- src/main/java/com/Service/LogicaLogReg.java
+++ src/main/java/com/Service/LogicaLogReg.java
@@ -1,30 +1,22 @@
 package com.Service;
 import com.GestioneDB.GestioneUtente;
 import com.gallery.gui.ValidazioneInput;
+//classe che fa da tramite tra  "lato grafico" e lato "gestionalr"
+
 
 public class LogicaLogReg {
 
+    private final GestioneUtente gestioneUtente = new GestioneUtente();
+    private final ValidazioneInput validazioneInput = new ValidazioneInput();
 
-        private final GestioneUtente gestioneUtente = new GestioneUtente();
-        private final ValidazioneInput validazioneInput = new ValidazioneInput();
-
-        public boolean effettuaLogin(String email, String password) {
-            // Validazione dei dati Log
-            if (!validazioneInput.emailValida(email) || !validazioneInput.passwordValida(password)) {
-                return false;
-            }
-            // Verifica credenziali
-            return gestioneUtente.verificaCredenziali(email, password);
-        }
-
-        public boolean effettuaRegistrazione(String username, String email, String password) {
-            // Validazione dei dati Reg
+    public boolean verificaSintassiReg(String username, String email, String password) {
+            // Se il "check" sulla sintassi è valido mando al db per la verifica(se c'è già)
             if (!validazioneInput.usernameValido(username) ||
                     !validazioneInput.emailValida(email) ||
                     !validazioneInput.passwordValida(password)) {
                 return false;
             }
-            // Registra utente
+            // Mando l'utente al gestore del db
             return gestioneUtente.verificaERegistraUtente(username, email, password);
         }
     }
