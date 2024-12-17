5bded570afcb9662c149b97e96ad6b9b2f8c32fa
Mario <mario.ranalli@student.univaq.it>
2024-12-17 03:08:37 +0100

    alert + modifica goProfile per portarsi infoprof

--- src/main/java/com/gallery/gui/MenuPrincipaleController.java
+++ src/main/java/com/gallery/gui/MenuPrincipaleController.java
@@ -1,10 +1,13 @@
 package com.gallery.gui;
 
+import com.entity.User;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MenuPrincipaleController {
+
+    private AlertInfo alert = new AlertInfo();
 
 
     private ProfiloApplication profilo = new ProfiloApplication();
@@ -12,18 +15,29 @@ public class MenuPrincipaleController {
     @FXML
     private Text welcomeText;
 
-    @FXML
-    private void goToProfile() {
-       profilo.openProfile();
-        Stage stage = (Stage) welcomeText.getScene().getWindow();
-        stage.close();
-    }
+    private User currentUser;
+
 
     @FXML
     private void exitApp() {
         // Chiudi l'applicazione
         Stage stage = (Stage) welcomeText.getScene().getWindow();
         stage.close();
+    }
+    @FXML
+    private void goToProfile() {
+        if (currentUser != null) {
+            profilo.openProfile(currentUser); // Passa l'utente al profilo
+            Stage stage = (Stage) welcomeText.getScene().getWindow();
+            stage.close();
+        } else {
+
+            alert.showAlertErrore("Errore", "Nessun utente loggato.");
+        }
+    }
+
+    public void setUser(User user) {
+        this.currentUser = user;
     }
 
 
